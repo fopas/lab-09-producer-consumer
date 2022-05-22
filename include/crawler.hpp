17624255bedb7df247b733edcee8e879f3ff3bae
Artem Sharov <ash.03@yandex.ru>
2022-05-22 11:48:54 -0700

    final

---
+++ include/crawler.hpp
@@ -0,0 +1,19 @@
+// Copyright 2022 Your Name <your_email>
+
+#ifndef INCLUDE_CRAWLER_HPP_
+#define INCLUDE_CRAWLER_HPP_
+
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/program_options.hpp>
+#include <iostream>
+#include <string>
+
+#include "consumer.hpp"
+#include "producer.hpp"
+
+namespace po = boost::program_options;
+
+void crawler(std::string url, unsigned depth, Consumer& k, Producer &p,
+             std::string outp, Queue &parser_queue);
+
+#endif  // INCLUDE_CRAWLER_HPP_
